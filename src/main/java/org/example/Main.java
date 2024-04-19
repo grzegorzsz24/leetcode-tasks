4054514505500a982e7b2486020578082b3522bd
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-04-19 10:19:21 +0200

    Solution for 290. Word Pattern

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -89,4 +89,23 @@ public class Main {
         }
         return true;
     }
+
+    public static boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        Map<Character, String> map = new HashMap<>();
+
+        if (pattern.length() != words.length) return false;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (map.containsKey(pattern.charAt(i))) {
+                String value = map.get(pattern.charAt(i));
+                if (!value.equals(words[i])) return false;
+            } else if (map.containsValue(words[i])){
+                return false;
+            } else {
+                map.put(pattern.charAt(i), words[i]);
+            }
+        }
+        return true;
+    }
 }
