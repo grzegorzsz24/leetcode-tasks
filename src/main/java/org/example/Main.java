f8a4ea67fa6b6fb66cdc766b4f9703a5fa5791b6
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-04-29 14:24:50 +0200

    Solution for 141. Linked List Cycle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -182,4 +182,19 @@ public class Main {
         }
         return sStack.isEmpty();
     }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
