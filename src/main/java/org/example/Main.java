9c85ca7eb0c28a9b31a5862cdbf5b48696703af7
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-06 17:55:04 +0200

    Solution for 100. Same Tree

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -203,4 +203,13 @@ public class Main {
 
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+
+        if (p != null && q != null && p.val == q.val) {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
 }
