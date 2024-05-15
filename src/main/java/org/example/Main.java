fe17573e69c493d93402c6f2c826cf5d3456d31d
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-15 17:34:45 +0200

    Solution for 539. Minimum absolute difference in BST

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -278,4 +278,24 @@ public class Main {
         }
         return ans;
     }
+
+    public int getMinimumDifference(TreeNode root) {
+        int ans = Integer.MAX_VALUE;
+        int prev = -1;
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (prev >= 0)
+                ans = Math.min(ans, root.val - prev);
+            prev = root.val;
+            root = root.right;
+        }
+
+        return ans;
+    }
 }
