8fd3834407f9e496725fe8e7e7420d0c9644cbe8
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-05-17 11:36:54 +0200

    Solution for 67. Add Binary

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -313,4 +313,22 @@ public class Main {
         root.right = CreateBST(nums, mid + 1, r);
         return root;
     }
+
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry == 1)
+        {
+            if(i >= 0)
+                carry += a.charAt(i--) - '0';
+            if(j >= 0)
+                carry += b.charAt(j--) - '0';
+            sb.append(carry % 2);
+            carry /= 2;
+        }
+        return sb.reverse().toString();
+    }
 }
