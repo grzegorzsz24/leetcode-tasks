58b578a6f0e4b8ba833e7b6053f30350ecd8247a
grzegorzsz24 <grzesio.szymanek@gmail.com>
2024-06-03 18:16:44 +0200

    Solution for 189. Rotate Array

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -378,4 +378,22 @@ public class Main {
         }
         return l;
     }
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverseArray(nums, 0, n - 1);
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, n - 1);
+    }
+
+    public void reverseArray(int [] nums, int beginIndex, int endIndex) {
+        while (beginIndex < endIndex) {
+            int temp = nums[beginIndex];
+            nums[beginIndex] = nums[endIndex];
+            nums[endIndex] = temp;
+            beginIndex++;
+            endIndex--;
+        }
+    }
 }
